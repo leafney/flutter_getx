0d38d089dff67b5d711b3df1715363f14faaf318
leafney <babycoolzx@126.com>
2021-02-09 15:15:33 +0800

    优化

---
+++ lib/home.dart
@@ -0,0 +1,20 @@
+/*
+ * @Author: leafney
+ * @Date: 2021-02-09 14:57:00
+ * @LastEditTime: 2021-02-09 15:01:25
+ * @FilePath: /flutter_getx/lib/home.dart
+ * @Description: 
+ */
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter GetX Demo'),
+      ),
+    );
+  }
+}
